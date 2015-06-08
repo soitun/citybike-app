6266793fa9f5d40a5f7db5d2a34ae6c0d9c2ab24
Tomasz Szulc <mail@szulctomasz.com>
2015-06-08 23:30:13 +0200

    Integrate CCHMapClusterController.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CCHMapClusterController
+#define COCOAPODS_POD_AVAILABLE_CCHMapClusterController
+#define COCOAPODS_VERSION_MAJOR_CCHMapClusterController 1
+#define COCOAPODS_VERSION_MINOR_CCHMapClusterController 6
+#define COCOAPODS_VERSION_PATCH_CCHMapClusterController 6
+
