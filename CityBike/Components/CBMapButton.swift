35e1db7fcbe6292afb6b06413ba1c06d23777f44
Tomasz Szulc <mail@szulctomasz.com>
2015-06-19 21:46:33 +0200

    #20 Implement bounce effect on CBMapButton.

--- CityBike/Components/CBMapButton.swift
+++ CityBike/Components/CBMapButton.swift
@@ -11,7 +11,24 @@ import UIKit
 class CBMapButton: CBButton {
 
     final override func makePressedStyle() {
-        self.alpha = 0.4
+        self.alpha = 0.8
+        self.bounce()
+    }
+    
+    private func bounce() {
+        let duration = 0.1
+        
+        UIView.animateWithDuration(duration, animations: {
+            self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.2, 1.2)
+            
+            }) { _ in
+                UIView.animateWithDuration(duration, animations: {
+                    self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.9, 0.9)
+                    
+                    }) { _ in
+                        UIView.animateWithDuration(duration, animations: { _ in self.transform = CGAffineTransformIdentity })
+                }
+        }
     }
     
     final override func makeNormalStyle() {
