29efc37bf236093594fbc1f901e96ff94fee4305
Tomasz Szulc <mail@szulctomasz.com>
2015-06-20 13:31:57 +0200

    Improve CBMapDetailView to have more control of view updates.

--- CityBike/Components/CBMapDetailView.swift
+++ CityBike/Components/CBMapDetailView.swift
@@ -30,6 +30,9 @@ class CBMapDetailView: UIView {
     @IBOutlet weak var distanceLabel: UILabel!
     
     private var dateFormatter: NSDateFormatter!
+    private var previousDistance: Float = 0.0
+    private var previousBikes = 0
+    private var previousSlots = 0
     
     
     @IBAction func closePressed(sender: AnyObject) {
@@ -52,27 +55,36 @@ class CBMapDetailView: UIView {
     }
     
     func update(text: String, detailText: String, freeBikes: Int, freeSlots: Int, distance: Float, date: NSDate) {
-       
-        self.labelTop.text = text.uppercaseString
-        self.labelDetails.text = detailText.uppercaseString
-        self.bikesLabel.text = "\(freeBikes)"
-        self.slotsLabel.text = "\(freeSlots)"
-        self.distanceLabel.text = String(format: "%0.2fkm", arguments: [distance / 1000.0])
-        
         func colorForValue(value: Int, min: Int, max: Int) -> UIColor {
             if value > max { return UIColor.plentyColor() }
             else if value == 0 { return UIColor.noneColor() }
             else { return UIColor.fewColor() }
         }
         
-        self.bikesImageView.tintColor = colorForValue(freeBikes, 0, 5)
-        self.slotsImageView.tintColor = colorForValue(freeSlots, 0, 5)
+        self.labelTop.text = text.uppercaseString
+        self.labelDetails.text = detailText.uppercaseString
+        
+        let newDistance = distance / 1000.0
+        if fabs(newDistance - previousDistance) > 0.05 {
+            previousDistance = newDistance
+            self.distanceLabel.text = String(format: "%0.2fkm", newDistance)
+            self.distanceContainer.bounce(0.1)
+        }
+        
+        if freeBikes != previousBikes {
+            previousBikes = freeBikes
+            self.bikesImageView.tintColor = colorForValue(freeBikes, 0, 5)
+            self.bikesLabel.text = "\(freeBikes)"
+            self.bikesImageView.bounce(0.1)
+        }
+        
+        if freeSlots != previousSlots {
+            previousSlots = freeSlots
+            self.slotsImageView.tintColor = colorForValue(freeSlots, 0, 5)
+            self.slotsLabel.text = "\(freeSlots)"
+            self.slotsImageView.bounce(0.1)
+        }
         
         self.labelThird.text = "UPDATED \(self.dateFormatter.stringFromDate(date))".uppercaseString
-        
-        // bounce
-        self.bikesImageView.bounce(0.1)
-        self.slotsImageView.bounce(0.1)
-        self.distanceContainer.bounce(0.1)
     }
 }
